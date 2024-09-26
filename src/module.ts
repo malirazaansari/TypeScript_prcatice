e5fd1ff6cc744b472412a69939c99922992ba557
Ali raza <malirazaansari4@gmail.com>
2024-09-27 02:29:15 +0500

    modules in typescript done now implementation with js node and react reamaining

---
+++ src/module.ts
@@ -0,0 +1,5 @@
+// import { Circle } from "./circle.ts"
+// const Circle = require"./circle.ts"
+// import Circle from "./circle.ts" for default export or only single class import from file
+// import * as Shapes from './circle.ts as wildcard export ........   Shape.Circle now we can access everything with .
+// re exporting.... 1st if there is too many modules than import them to speartor file and than that file import in index file
