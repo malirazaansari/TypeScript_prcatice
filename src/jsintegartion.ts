3bc1fdee8f7b2e53be124bea46a962d84842f3f0
Ali raza <malirazaansari4@gmail.com>
2024-09-28 02:30:34 +0500

    integration with js of ts done.... react and node is almost same..... TS cosrse done

---
+++ src/jsintegartion.ts
@@ -0,0 +1,8 @@
+//type check by
+//   @ts-no check
+//describing types using JSdocs
+//   /** */ here we write the props types and return type of all those thing that we used
+//decalring type.d.ts files
+//  here we make new files and declare every type that we using and import in js file
+//difinetly typed declartion files
+//  here we use already builtin types in github repo for libarires like "lodash" and use them in js file and no make byself decalration file
